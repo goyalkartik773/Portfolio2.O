7c81250d67c3cf2436d17ccbe6820fe6d27235fb
Kartik Goel <goyalkartik773@gmail.com>
2026-04-26 13:48:59 +0530

    fixes done

--- components/hero.tsx
+++ components/hero.tsx
@@ -8,7 +8,7 @@ import { SiLeetcode, SiCodeforces, SiCodechef } from "react-icons/si";
 const socialLinks = [
   { href: "https://www.linkedin.com/in/kartik-goel-a95b2630b/", icon: Linkedin, label: "LinkedIn" },
   { href: "https://github.com/goyalkartik773", icon: Github, label: "GitHub" },
-  { href: "https://leetcode.com/u/Kartikgoel773/", icon: SiLeetcode, label: "LeetCode" },
+  { href: "https://leetcode.com/u//", icon: SiLeetcode, label: "LeetCode" },
   { href: "https://codeforces.com/profile/goyalkartik773", icon: SiCodeforces, label: "Codeforces" },
   { href: "https://www.codechef.com/users/", icon: SiCodechef, label: "CodeChef" },
 ];
